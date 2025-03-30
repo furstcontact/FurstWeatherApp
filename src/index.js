3cf7f215d67c133725b41083fbe92f568d3f878d
furstcontact <furstcontact@gmail.com>
2025-03-30 12:41:22 +1030

    add basic search engine js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hello and Good day!");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let weatherCityElement = document.querySelector("#weather-city");
+  weatherCityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
