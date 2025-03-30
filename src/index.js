5c9be351d3b65935de6808dee2321217ea911a6a
furstcontact <furstcontact@gmail.com>
2025-03-30 13:21:16 +1030

    search function using api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let weatherCityElement = document.querySelector("#weather-city");
+  weatherCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "ob891t57a434d702dd2b3463eb606aff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let weatherCityElement = document.querySelector("#weather-city");
-  weatherCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
